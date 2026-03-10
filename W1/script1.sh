e4bf97ce031c1c1e4fee78a9e2e18895b378f6ae
MiliTheGoat <s225102@dtu.dk>
2026-03-10 20:00:07 +0100

    Ex2

--- W1/script1.sh
+++ W1/script1.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
-#BSUB -J sleeper
+# embedded options to bsub - start with #BSUB
+# -- our name ---
+#BSUB -J SingleCorePython 
+# -- choose queue --
 #BSUB -q hpc
+# -- specify that we need 4GB of memory per core/slot --
+# so when asking for 4 cores, we are really asking for 4*4GB=16GB of memory 
+# for this job. 
+#BSUB -R "rusage[mem=4GB]"
+# -- Notify me by email when execution begins --
+#BSUB -B
+# -- Notify me by email when execution ends   --
+#BSUB -N
+# -- s225102@dtu.dk -- 
+# please uncomment the following line and put in your e-mail address,
+# if you want to receive e-mail notifications on a non-default address
+##BSUB -u s225102@dtu.dk
+# -- Output File --
+#BSUB -o Output_%J.out
+# -- Error File --
+#BSUB -e Output_%J.err
+# -- estimated wall clock time (execution time): hh:mm -- 
 #BSUB -W 2
-#BSUB -R ”rusage[mem=512MB]”
-#BSUB –n 4
-#BSUB –R ”span[hosts=1]
-#BSUB -o sleeper_%J.out
-#BSUB -e sleeper_%J.err
-sleep 60
+# -- Number of cores requested -- 
+#BSUB -n 1 
+# -- Specify the distribution of the cores: on a single node --
+#BSUB -R "span[hosts=1]"
+# -- end of LSF options -- 
+
+source /dtu/projects/02613_2025/conda/conda_init.sh
+conda activate 02613
+
+python -u program1.py
