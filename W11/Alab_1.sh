f4d2e358b2b3a4f30781f1cb8217b92d8dabdb3f
MiliTheGoat <s225102@dtu.dk>
2026-05-04 19:28:40 +0200

    W11 work

---
+++ W11/Alab_1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#BSUB -J "Thajob[1-10]"
+#BSUB -q hpc
+#BSUB -R "rusage[mem=1GB]"
+#BSUB -n 1
+#BSUB -R "span[hosts=1]"
+#BSUB -W 00:10
+#BSUB -o output/array_%I_%J.out
+#BSUB -e output/array_%I_%J.err
+
+mkdir -p output
+
+echo "Job array index : $LSB_JOBINDEX"
+echo "Job ID          : $LSB_JOBID"
+echo "Host            : $(hostname)"
